21779d03f26f01d66c88540ef0742ae5f2c79e07
HeCG95 <jacktanger@163.com>
2022-12-12 11:26:29 +0800

    [A] 2022-12-12 11:19:30

---
+++ java-agent/jdk-asm-agent/src/main/java/vertx/fun/agent/TestAg.java
@@ -0,0 +1,18 @@
+package vertx.fun.agent;
+
+/**
+ * @author HeCG
+ * @description xxx
+ * @date 2022/12/12 11:20
+ * @since 1.0
+ */
+public class TestAg {
+
+    public static void main(String[] args) {
+
+        // -javaagent:E:\obank\code\github\HeCG95\jvm\learn-jvm\java-agent\jdk-asm-agent\target\ja-netfilter-jar-with-dependencies.jar
+        System.out.println("AAAAAAAAAAAAA");
+
+    }
+
+}
